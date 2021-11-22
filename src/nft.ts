ddaa1a3b615d9e1fe2e25b858d17e5af9b63d50e
Artizno <artiz@ecio.space>
2021-11-23 00:24:57 +0700

    Convert json data to object list

--- src/nft.ts
+++ src/nft.ts
@@ -65,32 +65,13 @@ export function isMint(event: Transfer): boolean {
   return event.params.from.toHexString() == Null
 }
 
-export function searchData(prefix: string, code: string): Data {
-  for (let index: number = 0; index < partData.length; index++) {
+export function searchData(prefix: string, code: string): Data | null {
+  for (let index = 0; index < partData.length; index++) {
     if (prefix + code == partData[index].CODE) {
       return partData[index];
     }
   }
-  return {
-    "CODE": "",
-    "RARITY": "",
-    "ELEMENT": "",
-    "NAME": "",
-    "ABILITY": "",
-    "INFORMATION": "",
-    "HP": 0,
-    "ATK": 0,
-    "DEF": 0,
-    "ASPD": 0,
-    "RANGE": 0,
-    "BONUS_HP": 0,
-    "BONUS_ATK": 0,
-    "BONUS_DEF": 0,
-    "BONUS_ASPD": 0,
-    "CRIT": 0,
-    "DODGE": 0,
-    "LIFESTEAL": 0
-  }
+  return null
 }
 
 export function handleTransfer(event: Transfer): void {
@@ -126,18 +107,13 @@ export function handleTransfer(event: Transfer): void {
   //Assign NFT Type
   nft.nftType = getNFTType(nft.partCode)
 
-
-  //Assign Level
-  if (nft.nftType == "card") {
-    nft.level = BigInt.fromI32(getLevel(nft.partCode))
-  }
-
   //Assign Class
   //limitation
 
   //Assign Part
   if (nft.nftType == "card") {
-    nft.level = BigInt.fromI32(getLevel(nft.partCode))
+
+    nft.level = getLevel(nft.partCode)
     // let code0 = GetCode(nft.partCode, 0);
     // let code1 = GetCode(nft.partCode, KINGDOM);
     nft.tribe = getTribe(nft.partCode)
@@ -173,29 +149,20 @@ export function handleTransfer(event: Transfer): void {
     nft.rarity = getEquipmentRarity(nft.partCode, nft.part)
 
     //Assign Card's Class 
-    let prefix: string
+    let prefix: string = ""
     let code: string = GetCode(nft.partCode, EQUIPMENT);
-    switch (nft.part) {
-      case "gear":
-        prefix = "SG"
-        break;
-      case "drone":
-        prefix = "SD"
-        break;
-      case "suite":
-        prefix = "SS"
-        break;
-      case "bot":
-        prefix = "SB"
-        break;
-      case "weapon":
-        prefix = "SW"
-        break;
-      default:
-        prefix = ""
-        break;
-    }
-
+    if (nft.part == "gear") {
+      prefix = "SG"
+    } else if (nft.part == "drone") {
+      prefix = "SD"
+    } else if (nft.part == "suite") {
+      prefix = "SS"
+    } else if (nft.part == "bot") {
+      prefix = "SB"
+    } else if (nft.part == "weapon") {
+      prefix = "SW"
+    } 
+ 
     // Assign Class
     nft.nftClass = getNFTClass(prefix, code);
 
@@ -223,40 +190,41 @@ export function handleTransfer(event: Transfer): void {
 // suite
 // bot
 // weapon
-export function getEquipmentRarity(code: string, equipment: string): string {
-  let prefix: string
-  switch (equipment) {
-    case GEAR_CASE:
-      prefix = "SG"
-      break;
-    case "drone":
-      prefix = "SD"
-      break;
-    case "suite":
-      prefix = "SS"
-      break;
-    case "bot":
-      prefix = "SB"
-      break;
-    case WEAPON_CASE:
-      prefix = "SW"
-      break;
-    default:
-      prefix = ""
-      break;
+
+export function getEquipmentRarity(code: string, equipment: string | null): string {
+
+  let gear: string = "gear";
+  let drone: string = "drone";
+  let suite: string = "suite";
+  let bot: string = "bot";
+  let weapon: string = "weapon";
+  let prefix: string = "";
+
+
+  if (equipment == "gear") {
+    prefix = "SG"
+  } else if (equipment == "drone") {
+    prefix = "SD"
+  } else if (equipment == "suite") {
+    prefix = "SS"
+  } else if (equipment == "bot") {
+    prefix = "SB"
+  } else if (equipment == "weapon") {
+    prefix = "SW"
   }
 
-  for (let index: number = 0; index < partData.length; index++) {
+
+  for (let index = 0; index < partData.length; index++) {
     const pd = partData[index];
     if (pd.CODE == prefix + code) {
-      return partData[index].RARITY.toLowerCase()
+      return pd.RARITY.toLowerCase()
     }
   }
   return ""
 }
 
 export function getNFTName(prefix: string, code: string): string {
-  for (let index: number = 0; index < partData.length; index++) {
+  for (let index = 0; index < partData.length; index++) {
     const pd = partData[index];
     if (pd.CODE == prefix + code) {
       return partData[index].NAME.toLowerCase()
@@ -267,7 +235,7 @@ export function getNFTName(prefix: string, code: string): string {
 
 export function getNFTClass(prefix: string, code: string): string {
 
-  for (let index: number = 0; index < partData.length; index++) {
+  for (let index = 0; index < partData.length; index++) {
     const pd = partData[index];
     if (pd.CODE.toString() == (prefix + code).toString()) {
       return partData[index].ELEMENT.toLowerCase()
@@ -277,7 +245,7 @@ export function getNFTClass(prefix: string, code: string): string {
 }
 export function getCardRarity(code: string): string {
 
-  for (let index: number = 0; index < partData.length; index++) {
+  for (let index = 0; index < partData.length; index++) {
     const pd = partData[index];
     if (pd.CODE == "SH" + code) {
       return partData[index].RARITY.toLowerCase()
@@ -287,7 +255,7 @@ export function getCardRarity(code: string): string {
 }
 
 export function getEquipment(partCode: string): string {
-  let code: string = GetCode(partCode, EQUIPMENT);
+  //let code: string = GetCode(partCode, EQUIPMENT);
   // gear
   // drone
   // suite
@@ -298,70 +266,68 @@ export function getEquipment(partCode: string): string {
   // export const SUITE_PART = 5
   // export const BOT_PART = 6
   // export const WEAPON_PART = 8
-  switch (code) {
-    case "03":
-      return "gear"
-    case "04":
-      return "drone"
-    case "05":
-      return "suite"
-    case "06":
-      return "bot"
-    case "08":
-      return "weapon"
-    default:
-      return ""
-  }
+  // switch (code) {
+  //   case "03":
+  //     return "gear"
+  //   case "04":
+  //     return "drone"
+  //   case "05":
+  //     return "suite"
+  //   case "06":
+  //     return "bot"
+  //   case "08":
+  //     return "weapon"
+  //   default:
+  //     return ""
+  // }
+  return "SG"
 }
 
 export function getTribe(partCode: string): string {
   let code: string = GetCode(partCode, KINGDOM);
-  switch (code) {
-    case "00":
-      return "solarian"
-    case "01":
-      return "andromedian"
-    case "02":
-      return "scultpian"
-    case "03":
-      return "mercenary"
-    default:
-      return ""
+  if (code == "00") {
+    return "solarian"
+  } else if (code == "01") {
+    return "andromedian"
+  } else if (code == "02") {
+    return "scultpian"
+  } else if (code == "03") {
+    return "mercenary"
   }
+  return ""
 }
 export function getNFTType(partCode: string): string {
+
   let code: string = GetCode(partCode, 0);
-  switch (code) {
-    case "00": case "01": case "02": case "03": case "04": case "05":
-      return "fagment"
-    case "06":
-      return "card"
-    default:
-      return ""
+  if (code == "00" || code == "01" || code == "02" || code == "03" || code == "04" || code == "05") {
+    return "fagment"
+  } else if ("06") {
+    return "card"
   }
+
+  return ""
+
 }
 
-export function getLevel(partCode: string): number {
+export function getLevel(partCode: string): BigInt {
   let code: string = GetCode(partCode, 9);
-  switch (code) {
-    case "00":
-      return 1;
-    case "01":
-      return 2;
-    case "02":
-      return 3;
-    case "03":
-      return 4;
-    case "04":
-      return 5;
-    default:
-      return 0;
+  if (code == "00") {
+    return new BigInt(1);
+  } else if (code == "01") {
+    return new BigInt(2);
+  } else if (code == "02") {
+    return new BigInt(3);
+  } else if (code == "03") {
+    return new BigInt(4);
+  } else if (code == "04") {
+    return new BigInt(5);
   }
+  return new BigInt(0);
 }
 
 export function GetCode(partCode: string, no: number): string {
   var count: number = 0;
-  for (let i: number = partCode.length - 1; i >= 0; i--) {
+  for (let i = partCode.length - 1; i >= 0; i--) {
     if (count == no) {
       return `${partCode.at(i - 1)}${(partCode.at(i))}`
     }
@@ -370,71 +336,7 @@ export function GetCode(partCode: string, no: number): string {
   }
   return ""
 }
-
-
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  // let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // // Entities only exist after they have been saved to the store;
-  // // `null` checks allow to create entities on demand
-  // if (!entity) {
-  //   entity = new ExampleEntity(event.transaction.from.toHex())
-
-  //   // Entity fields can be set using simple assignments
-  //   entity.count = BigInt.fromI32(0)
-  // }
-
-  // BigInt and BigDecimal math are supported
-  // entity.count = entity.count + BigInt.fromI32(1)
-
-  // // Entity fields can be set based on event parameters
-  // entity.owner = event.params.owner
-  // entity.approved = event.params.approved
-
-  // // Entities can be written to the store with `.save()`
-  // entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.MINTER_ROLE(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.hasRole(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenInfo(...)
-  // - contract.tokenURI(...)
-}
-
-export function handleApprovalForAll(event: ApprovalForAll): void { }
-
-export function handleRoleAdminChanged(event: RoleAdminChanged): void { }
-
-export function handleRoleGranted(event: RoleGranted): void { }
-
-export function handleRoleRevoked(event: RoleRevoked): void { }
-
-
+ 
 export function createOrLoadAccount(id: Address): Account {
   let account = Account.load(id.toHex())
 

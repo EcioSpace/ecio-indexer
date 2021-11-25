e581de2becb1096eeeea551e158c4466ebf41bcd
Artizno <artiz@ecio.space>
2021-11-26 01:01:50 +0700

    Fix bug

--- src/marketplace.ts
+++ src/marketplace.ts
@@ -4,8 +4,7 @@ import {
   OrderCanceled,
   OrderSuccessful
 } from "../generated/Market/Market"
-import { NFT, Account, Order } from "../generated/schema"
-import { partData, Data } from "./data/part"
+import { NFT, Order } from "../generated/schema"
 import { Null } from './data/addresses'
 import { log } from '@graphprotocol/graph-ts'
 import { getNFTId } from "./modules/nft"
@@ -13,13 +12,15 @@ import { getNFTId } from "./modules/nft"
 export function handleOrderCreated(event: OrderCreated): void {
 
   const tokenId = event.params.tokenId
-  const nftId = getNFTId(event.address.toHexString(),event.params.tokenId.toString());
+  const nftId = getNFTId(event.params.nftContract.toHexString(),event.params.tokenId.toString());
+  log.debug("nftId:{}", [nftId]);
 
   let nft = NFT.load(nftId)
   if (nft != null) {
-
     let orderId = event.params.orderId.toString()
+    log.debug("orderId:{}", [orderId]);
     let order = new Order(orderId)
+    order.buyWithERC20Address =  event.params.buyWithTokenContract
     order.status = "open"  //  open sold cancelled
     order.nft = tokenId.toString()
     order.nftAddress = event.params.nftContract
@@ -31,14 +32,7 @@ export function handleOrderCreated(event: OrderCreated): void {
     order.blockNumber = event.block.number
     order.createdAt = event.block.timestamp
     order.updatedAt = event.block.timestamp
-
     order.save()
-
-    if (order.status == "open") {
-      order.status = "cancelled"
-      order.updatedAt = event.block.timestamp
-      order.save()
-    }
 
     nft.updatedAt = event.block.timestamp
     nft = updateNFTOrderProperties(nft, order)
@@ -50,7 +44,7 @@ export function handleOrderCreated(event: OrderCreated): void {
 
 export function handleOrderCanceled(event: OrderCanceled): void {
   
-  const nftId = getNFTId(event.address.toHexString(),event.params.tokenId.toString());
+  const nftId = getNFTId(event.params.nftContract.toHexString(),event.params.tokenId.toString());
 
   let orderId = event.params.orderId.toString()
 
@@ -72,9 +66,11 @@ export function handleOrderCanceled(event: OrderCanceled): void {
 
 export function handleOrderSuccessful(event: OrderSuccessful): void {
   
-  const nftId = getNFTId(event.address.toHexString(),event.params.tokenId.toString());
+  const nftId = getNFTId(event.params.nftContract.toHexString(),event.params.tokenId.toString());
+  log.debug("nftId:{}", [nftId]);
 
   let orderId = event.params.orderId.toString()
+  log.debug("orderId:{}", [orderId]);
 
   let order = Order.load(orderId)
   if (order == null) {
